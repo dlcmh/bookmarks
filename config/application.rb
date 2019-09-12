991727b43ff22558e25906df93dc080658c4f47c
David Chin <dlcmhd@me.com>
2019-09-12 10:02:30 +0800

    config.hosts bookmarks.briefnotes.net

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module Bookmarks
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # custom configs
+    config.hosts << 'bookmarks.briefnotes.net'
   end
 end
